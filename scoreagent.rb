fab7cfd4c99a61c7158d4f487a29644d8d918d87
James Josephson <jamesartj@gmail.com>
2014-05-12 14:31:07 -0700

    Writes file after edit

--- scoreagent.rb
+++ scoreagent.rb
@@ -1,25 +1,29 @@
 require 'aws'
 
 s3 = AWS::S3.new(
-  :access_key_id => 'abc',
-  :secret_access_key => '123'
+  :access_key_id => 'AKIAJJWCYVD6QPYXISGQ',
+  :secret_access_key => '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'
 )
 
 scorefile = 'score'
 bucketname = 'edurange'
+log = 'log'
 
-unless bucket.exists?
-  s3.buckets.create(bucketname)
+bucket = s3.buckets[bucketname]
 
-bucket = s3.buckets[bucketname] 
+s3.buckets.create(bucketname) if bucket.nil?
+
+scorefile = File.new(scorefile, 'w') unless File.exists?(scorefile)
+
 scoreagent = File.new(scorefile, 'r')
-contents = File.read(scorefile)
 mtime = scoreagent.mtime
 
 loop do
-  if scoreagent.mtime > mtime
-    S3Object.store(scorefile, open(scorefile), bucketname)
-    log = 'log'
+  if scoreagent.mtime >= mtime
+  	contents = File.read(scorefile)
+  	obj = bucket.objects[scorefile]
+  	bucket.objects.create(scorefile, open(scorefile)) unless obj.exists?
+  	obj.write(contents)
     File.open(log, 'a') { |log| log.puts(mtime.to_s + "\n" + contents + "\n") }
     mtime = scoreagent.mtime
   end
