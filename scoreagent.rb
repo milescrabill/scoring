4da8cd38bfff3feb0df8ff89b5559fbb12ae5398
Miles Crabill <miles@milescrabill.com>
2014-05-12 14:47:50 -0700

    when scorefile is update it in S3 buket

--- scoreagent.rb
+++ scoreagent.rb
@@ -7,19 +7,24 @@ s3 = AWS::S3.new(
 
 scorefile = 'score'
 bucketname = 'edurange'
+log = 'log'
 
-unless bucket.exists?
-  s3.buckets.create(bucketname)
+bucket = s3.buckets[bucketname]
 
-bucket = s3.buckets[bucketname] 
+s3.buckets.create(bucketname) if bucket.nil?
+
+scorefile = File.new(scorefile, 'w') unless File.exists?(scorefile)
 scoreagent = File.new(scorefile, 'r')
-contents = File.read(scorefile)
+logfile = File.new(log, 'w') unless File.exists?(log)
+
 mtime = scoreagent.mtime
 
 loop do
-  if scoreagent.mtime > mtime
-    S3Object.store(scorefile, open(scorefile), bucketname)
-    log = 'log'
+  if scoreagent.mtime >= mtime
+    contents = File.read(scorefile)
+    obj = bucket.objects[scorefile]
+    bucket.objects.create(scorefile, open(scorefile)) unless obj.exists?
+    obj.write(contents)
     File.open(log, 'a') { |log| log.puts(mtime.to_s + "\n" + contents + "\n") }
     mtime = scoreagent.mtime
   end
