0e728d9eb7caf80b089b04f415450a35c95efb37
Miles Crabill <miles@milescrabill.com>
2014-05-13 09:51:27 -0700

    Fixed logging repitition

--- scoreagent.rb
+++ scoreagent.rb
@@ -13,19 +13,19 @@ bucket = s3.buckets[bucketname]
 
 s3.buckets.create(bucketname) if bucket.nil?
 
-scorefile = File.new(scorefile, 'w') unless File.exists?(scorefile)
+scoreagent = File.new(scorefile, 'w') unless File.exists?(scorefile)
 scoreagent = File.new(scorefile, 'r')
 logfile = File.new(log, 'w') unless File.exists?(log)
 
-mtime = scoreagent.mtime
+mtime = Time.at(0)
 
 loop do
-  if scoreagent.mtime >= mtime
+  if scoreagent.mtime > mtime
+    mtime = scoreagent.mtime
     contents = File.read(scorefile)
     obj = bucket.objects[scorefile]
     bucket.objects.create(scorefile, open(scorefile)) unless obj.exists?
     obj.write(contents)
     File.open(log, 'a') { |log| log.puts(mtime.to_s + "\n" + contents + "\n") }
-    mtime = scoreagent.mtime
   end
 end
