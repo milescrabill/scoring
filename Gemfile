e5a79cfb8b60e4ea63cc13af401ae6714b78d8ae
Miles Crabill <miles@milescrabill.com>
2014-05-14 11:29:00 -0700

    started daemonizing

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 # gem "rails"
 gem 'aws-s3', :require => 'aws/s3'
 gem 'aws-sdk'
+gem 'daemons'
--- scoreagent.rb
+++ scoreagent.rb
@@ -1,13 +1,13 @@
-require 'aws'
+#!/usr/bin/env ruby
 
-s3 = AWS::S3.new(
-  :access_key_id => 'abc',
-  :secret_access_key => '123'
-)
+require "aws"
+require "daemons"
 
-scorefile = 'score'
-bucketname = 'edurange'
-log = 'log'
+s3 = AWS::S3.new
+
+scorefile = "score"
+bucketname = "edurange"
+log = "log"
 
 bucket = s3.buckets[bucketname]
 
@@ -19,13 +19,18 @@ logfile = File.new(log, 'w') unless File.exists?(log)
 
 mtime = Time.at(0)
 
-loop do
-  if scoreagent.mtime > mtime
-    mtime = scoreagent.mtime
-    contents = File.read(scorefile)
-    obj = bucket.objects[scorefile]
-    bucket.objects.create(scorefile, open(scorefile)) unless obj.exists?
-    obj.write(contents)
-    File.open(log, 'a') { |log| log.puts(mtime.to_s + "\n" + contents + "\n") }
+Daemons.run_proc(
+  'test-shit',
+  :log_output => true
+) do
+  loop do
+    if scoreagent.mtime > mtime
+      mtime = scoreagent.mtime
+      contents = File.read(scorefile)
+      obj = bucket.objects[scorefile]
+      bucket.objects.create(scorefile, open(scorefile)) unless obj.exists?
+      obj.write(contents)
+      File.open(log, 'a') { |log| log.puts(mtime.to_s + "\n" + contents + "\n") }
+    end
   end
 end
