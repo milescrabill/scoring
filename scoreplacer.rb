5ccea090f12369962867029e196606190ec56b36
Miles Crabill <miles@milescrabill.com>
2014-05-15 11:20:42 -0700

    tiny change to scoreplacer

--- scoreplacer.rb
+++ scoreplacer.rb
@@ -15,7 +15,12 @@ else
 	File.open(filename, 'r') { |file| @answers_doc = file.read }
 end
 
-File.open("scoring", 'w') { |file| file.write(@answers_doc) }
+File.open("/tmp/scoring", 'w') { |file| file.write(@answers_doc) }
 
 #make sure output file is answers seperated by \n
 #if user submits file without spaces then WRONG
+
+#./answer
+#hey enter your answers
+#10.1.1.3
+#10.5.3.2
