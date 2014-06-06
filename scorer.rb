06d1a667e6459527ecc795f84f8e92586fd3140e
Miles Crabill <miles@milescrabill.com>
2014-06-06 14:23:57 -0700

    changed daemon name of scoreagent, daemonized scorer

--- scorer.rb
+++ scorer.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+require 'daemons'
 require 'net/http'
 require 'uri'
 
@@ -5,19 +7,25 @@ def open(url)
   Net::HTTP.get(URI.parse(url))
 end
 
-points = 0
-
 answers = File.open("/tmp/answers", "r") { |f| f.read.split("\n") }
 scoring_pages = File.open("/tmp/scoring_pages", "r") { |f| f.read.split("\n") }
 
-submitted = []
-scoring_pages.each { |s| submitted << open(s) }
+Daemons.run_proc('edurange-scorer', :log_output => true) do
+  loop do
+    points = 0
 
-answers.each do |answer|
-  submitted.each_line do |submitted_line| 
-    if answer == submitted_line
-      points += 1
+    submitted = []
+    scoring_pages.each { |s| submitted << open(s) }
+
+    answers.each do |answer|
+      submitted.each_line do |submitted_line| 
+        if answer == submitted_line
+          points += 1
+      end
+    end
+
+    File.open("/tmp/points", "w+") { |f| f.write(points) }
+
+    sleep(0.5)
   end
 end
-
-puts points
