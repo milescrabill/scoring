8732bd67f15961b76e67b7269901176ccebd7e8d
Miles Crabill <miles@milescrabill.com>
2014-06-11 11:50:17 -0700

    fixed scorer, should be working

--- scorer.rb
+++ scorer.rb
@@ -8,29 +8,28 @@ end
 
 answers = File.open("/tmp/scoring/answers", "r") { |f| f.read.split("\n") }
 scoring_pages_url = File.open("/tmp/scoring/scoring_pages", "r") { |f| f.read }
-scoring_pages = open(scoring_pages_url).split("\n")
 
-Daemons.run_proc('edurange-scorer', :log_output => true) do
-  loop do
+# Daemons.run_proc('edurange-scorer', :log_output => true) do
+#   loop do
     points = 0
-
     submitted = []
-    
+
+    scoring_pages = open(scoring_pages_url).split("\n")
     scoring_pages.each { |s| submitted << open(s).split("\n") }
     submitted.flatten!
     submitted.each { |s| s.chomp }
 
     answers.each do |answer|
       submitted.each do |submitted_line|
-        File.open("/tmp/scoring/log", "w+") { |f| f.write("'" + answer + "'" + " : " + "'" + submitted_line + "'" + "\n") }
+        File.open("/tmp/scoring/log", "a+") { |f| f.write("'" + answer + "'" + " : " + "'" + submitted_line + "'" + "\n") }
         if answer == submitted_line
           points += 1
         end
       end
     end
 
-    File.open("/tmp/scoring/points", "w+") { |f| f.write(points + "\n") }
+    File.open("/tmp/scoring/points", "w+") { |f| f.write(points.to_s + "\n") }
 
     sleep(0.5)
-  end
-end
+#   end
+# end
