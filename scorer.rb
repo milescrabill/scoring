a073f867792039e04b3f17155603ad2483ffd7bc
Miles Crabill <miles@milescrabill.com>
2014-06-10 22:06:47 -0700

    updated scorer, quasi-working

--- scorer.rb
+++ scorer.rb
@@ -1,26 +1,30 @@
 #!/usr/bin/env ruby
 require 'daemons'
-require 'net/http'
-require 'uri'
+require 'open-uri'
 
 def open(url)
-  Net::HTTP.get(URI.parse(url))
+  URI.parse(url).read
 end
 
 answers = File.open("/tmp/scoring/answers", "r") { |f| f.read.split("\n") }
-scoring_pages = File.open("/tmp/scoring/scoring_pages", "r") { |f| f.read.split("\n") }
+scoring_pages_url = File.open("/tmp/scoring/scoring_pages", "r") { |f| f.read }
+scoring_pages = open(scoring_pages_url).split("\n")
 
 Daemons.run_proc('edurange-scorer', :log_output => true) do
   loop do
     points = 0
 
     submitted = []
-    scoring_pages.each { |s| submitted << open(s) }
+    
+    scoring_pages.each { |s| submitted << open(s).split("\n") }
+    submitted.flatten!
 
     answers.each do |answer|
-      submitted.each_line do |submitted_line| 
+      submitted.each do |submitted_line|
+        File.open("/tmp/scoring/log", "w+") { |f| f.write(answer + " : " + submitted_line + "\n") }
         if answer == submitted_line
           points += 1
+        end
       end
     end
 
